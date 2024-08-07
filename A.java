1d318d106325c1cc545aac890d7a64c4a77f22f2
Shilvant Kamble <shilvant358kamble@gmail.com>
2024-08-07 13:27:00 +0530

    initial commit

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A
+{
+public static void main(String args[])
+{ 
+  Systemout.println("Hello");
+}
+
+}
+
+Atomic habit
---
+++ C.java
@@ -0,0 +1,9 @@
+public class C{
+   public static void main(String arg[])
+    {
+       System.out.println("Name = Shilvant kamble");
+       System.out.println("Address = Jalgaon");
+       System.out.println("Email = shilvantkamble673@gmail.com");
+     }
+
+}
